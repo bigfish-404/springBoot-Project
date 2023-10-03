897d5d75b4790efabdeee93a9264230bdf39c64b
bigfish <bigfish.alert@gmail.com>
2023-10-03 15:12:46 +0900

    web-blog

---
+++ src/test/java/com/bigfish/blog/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigfish.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
